05968de05a38326fb1bd8f7ed28a034650b4a0e2
Scott McLeod <halcyonblue@gmail.com>
2014-02-08 02:00:29 -0500

    Revert "Attempt to fix faulty dashboards routing in MVC"
    This reverts commit e95c1e8e7777ac3dfe790d510b48cd43d4501132.

--- html/Dashboards-Body.ascx.cs
+++ html/Dashboards-Body.ascx.cs
@@ -61,9 +61,9 @@ public partial class Resources_Html_Dashboards_Body : UserControl
       foreach (ReportInfo reportInfo in cachedInfos)
         if (reportInfo.Dashboard) {
           if (String.IsNullOrEmpty(normalUrl))
-            normalUrl = "Dashboards?rn=" + HttpUtility.UrlEncode(reportInfo.FullName);
+            normalUrl = "Dashboards.aspx?rn=" + HttpUtility.UrlEncode(reportInfo.FullName);
           if (String.IsNullOrEmpty(demoUrl) && reportInfo.Name.ToLower() == "dashboard")
-            demoUrl = "Dashboards?rn=" + HttpUtility.UrlEncode(reportInfo.FullName);
+            demoUrl = "Dashboards.aspx?rn=" + HttpUtility.UrlEncode(reportInfo.FullName);
         }
       if (!String.IsNullOrEmpty(demoUrl))
         Response.Redirect(demoUrl);
